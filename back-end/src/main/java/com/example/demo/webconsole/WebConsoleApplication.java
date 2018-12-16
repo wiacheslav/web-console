6263eefad17d33412d2596aac993df44b4242e1d
Viacheslav <scrynkaa@gmail.com>
2018-12-16 21:11:17 +0200

    initial commit

---
+++ back-end/src/main/java/com/example/demo/webconsole/WebConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.webconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebConsoleApplication.class, args);
+    }
+}
